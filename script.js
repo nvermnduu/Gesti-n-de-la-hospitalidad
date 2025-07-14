228d7eec651d0eb809201da39c0ace19d00342a7
nvermnduu <aimetp1962@gmail.com>
2025-07-13 22:03:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function toggleDetalle(id) {
+  const elemento = document.getElementById(id);
+  if (elemento.style.display === 'none' || elemento.style.display === '') {
+    elemento.style.display = 'block';
+  } else {
+    elemento.style.display = 'none';
+  }
+}
